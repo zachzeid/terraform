3672b384af6ccf8a76f0d5d2c2acc2fb253ee4f7
zach.zeid <zach.zeid@gmail.com>
2018-08-21 08:41:23 -0400

    initial commit, main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "google" {
+  credentials = "${file("herpaderp-dd00d52e1429.json")}"
+  project = "herpaderp-1"
+  region = "us-central1"
+}
+
+resource "google_compute_instance" "sshserver" {
+  name = "sshserver1"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-b"
+  boot_disk = {
+    initialize_params = {
+      image = "debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+
+}
