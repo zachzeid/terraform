26c8d3fd0494b0f7437938661418b70bb1f418f8
zach.zeid <zach.zeid@gmail.com>
2018-08-21 16:10:24 -0400

    Added terraform backend

--- providers.tf
+++ providers.tf
@@ -3,11 +3,16 @@ provider "google" {
   project = "herpaderp-1"
   region = "us-central1"
 }
+terraform {
+  backend "gcs" {
+    bucket = "terraform-remote-state-5c1f261e-f412-4e1e-8907-63ba259ec6f5"
+  }
+}
 data "terraform_remote_state" "cloudbuilder" {
   backend = "gcs"
   config {
     bucket = "terraform-remote-state-5c1f261e-f412-4e1e-8907-63ba259ec6f5"
-      key = "test/terraform.tfstate"
+    key = "test/terraform.tfstate"
     prefix = "test"
   }
 }
