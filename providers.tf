3bac19deeeb7674eaed7ccdbe5dce6fc6b954c05
zach.zeid <zach.zeid@gmail.com>
2018-08-21 17:18:34 -0400

    remove state

--- providers.tf
+++ providers.tf
@@ -5,16 +5,6 @@ provider "google" {
 }
 terraform {
   backend "gcs" {
-    project = "herpaderp-1"
     bucket = "terraform-remote-state-5c1f261e-f412-4e1e-8907-63ba259ec6f5"
-    prefix = "test"
-    credentials = "herpaderp-dd00d52e1429.json"
-  }
-}
-data "terraform_remote_state" "cloudbuilder" {
-  backend = "gcs"
-  config {
-    bucket = "terraform-remote-state-5c1f261e-f412-4e1e-8907-63ba259ec6f5"
-    prefix = "test"
   }
 }
