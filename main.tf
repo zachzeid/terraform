f16b36936c10c3000349fb3749ba3abd3d12f76f
zach.zeid <zach.zeid@gmail.com>
2018-08-21 09:27:08 -0400

    Added in tags

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "google_compute_instance" "sshserver" {
   name = "sshserver1"
   machine_type = "n1-standard-1"
   zone = "us-central1-b"
+  tags = ["deployed from", "CICD"]
   boot_disk = {
     initialize_params = {
       image = "debian-9"
