3f4eb2ac0bcdb2921ef54c463f603b62a622495c
zach.zeid <zach.zeid@gmail.com>
2018-08-21 16:40:02 -0400

    trying to add credentials to backend

--- providers.tf
+++ providers.tf
@@ -5,7 +5,10 @@ provider "google" {
 }
 terraform {
   backend "gcs" {
+    project = "herpaderp-1"
     bucket = "terraform-remote-state-5c1f261e-f412-4e1e-8907-63ba259ec6f5"
+    prefix = "test"
+    credentials = "/herpaderp-dd00d52e1429.json"
   }
 }
 data "terraform_remote_state" "cloudbuilder" {
