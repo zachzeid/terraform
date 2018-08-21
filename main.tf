1f89c88bd8eeecdc632fb82f4abce6b66ff1fd5e
zach.zeid <zach.zeid@gmail.com>
2018-08-21 16:04:25 -0400

    changed server.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ variable "from" {
   default = "cloudbuilder"
 }
 resource "google_compute_instance" "sshserver" {
-  name = "sshserver1"
+  name = "sshserver"
   machine_type = "n1-standard-1"
   zone = "us-central1-b"
   labels = {
